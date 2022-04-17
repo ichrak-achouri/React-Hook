29e9c50c4c7065711febe95638b6687b92a62308
ichrak-achouri <formation@ihs.com.tn>
2022-04-17 18:41:11 +0200

    reacthook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ContainerMovie from './movieContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <ContainerMovie/>
+    </div>
+  );
+}
+
+export default App;
